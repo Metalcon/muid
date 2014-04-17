033af8bcd184ca597ae7308f975a74f83eae1967
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-04-17 15:54:20 +0200

    added entity types: Disc, Gallery, Image

--- src/main/java/de/metalcon/domain/UidType.java
+++ src/main/java/de/metalcon/domain/UidType.java
@@ -11,8 +11,11 @@ import de.metalcon.domain.helper.UnknownMuidException;
  * <ul>
  * <li>band</li>
  * <li>city</li>
+ * <li>disc</li>
  * <li>event</li>
+ * <li>gallery</li>
  * <li>genre</li>
+ * <li>image</li>
  * <li>instrument</li>
  * <li>record</li>
  * <li>tour</li>
@@ -23,167 +26,185 @@ import de.metalcon.domain.helper.UnknownMuidException;
  * </ul>
  */
 public enum UidType {
-	/**
-	 * band linked to a genre
-	 */
-	BAND("band", (short) 0),
+    /**
+     * band linked to a genre
+     */
+    BAND("band", (short) 0),
 
-	/**
-	 * city
-	 */
-	CITY("city", (short) 1),
+    /**
+     * city
+     */
+    CITY("city", (short) 1),
 
-	/**
-	 * event hosted in a venue
-	 */
-	EVENT("event", (short) 2),
+    /**
+     * disc of a record
+     */
+    DISC("disc", (short) 11),
 
-	/**
-	 * metal genre
-	 */
-	GENRE("genre", (short) 3),
+    /**
+     * event hosted in a venue
+     */
+    EVENT("event", (short) 2),
 
-	/**
-	 * music instrument
-	 */
-	INSTRUMENT("instrument", (short) 4),
+    /**
+     * gallery (manually) created by an entity
+     */
+    GALLERY("gallery", (short) 12),
 
-	/**
-	 * record of a band
-	 */
-	RECORD("record", (short) 5),
+    /**
+     * metal genre
+     */
+    GENRE("genre", (short) 3),
 
-	/**
-	 * collection of events
-	 */
-	TOUR("tour", (short) 6),
+    /**
+     * image linked to entities
+     */
+    IMAGE("image", (short) 13),
 
-	/**
-	 * record track
-	 */
-	TRACK("track", (short) 7),
+    /**
+     * music instrument
+     */
+    INSTRUMENT("instrument", (short) 4),
 
-	/**
-	 * user
-	 */
-	USER("user", (short) 8),
+    /**
+     * record of a band
+     */
+    RECORD("record", (short) 5),
 
-	/**
-	 * venue located in a city
-	 */
-	VENUE("venue", (short) 9),
+    /**
+     * collection of events
+     */
+    TOUR("tour", (short) 6),
 
-	/**
-	 * external URL
-	 */
-	URL("url", (short) 10);
+    /**
+     * record track
+     */
+    TRACK("track", (short) 7),
 
-	/**
-	 * Map to map identy type names (strings) to enums
-	 */
-	static TreeMap<String, UidType> allTypesByString = new TreeMap<String, UidType>();
+    /**
+     * user
+     */
+    USER("user", (short) 8),
 
-	/**
-	 * Fill allTypesByString
-	 */
-	static {
-		for (UidType type : UidType.values()) {
-			allTypesByString.put(type.getIdentifier(), type);
-		}
-	}
+    /**
+     * venue located in a city
+     */
+    VENUE("venue", (short) 9),
 
-	/**
-	 * identifier of the Muid type
-	 */
-	private final String identifier;
+    /**
+     * external URL
+     */
+    URL("url", (short) 10);
 
-	/**
-	 * Raw identifier of the Muid type
-	 */
-	private final short rawValue;
+    /**
+     * Map to map identy type names (strings) to enums
+     */
+    static TreeMap<String, UidType> allTypesByString =
+            new TreeMap<String, UidType>();
 
-	/**
-	 * create a new Muid type
-	 * 
-	 * @param identifier
-	 *            identifier of the Muid type
-	 */
-	private UidType(final String identifier, final short rawValue) {
-		this.identifier = identifier;
-		this.rawValue = rawValue;
-	}
+    /**
+     * Fill allTypesByString
+     */
+    static {
+        for (UidType type : UidType.values()) {
+            allTypesByString.put(type.getIdentifier(), type);
+        }
+    }
 
-	/**
-	 * @return identifier of the Muid type
-	 */
-	public String getIdentifier() {
-		return identifier;
-	}
+    /**
+     * identifier of the Muid type
+     */
+    private final String identifier;
 
-	/**
-	 * @return short value identifying the Muid type
-	 */
-	public short getRawIdentifier() {
-		return rawValue;
-	}
+    /**
+     * Raw identifier of the Muid type
+     */
+    private final short rawValue;
 
-	@Override
-	public String toString() {
-		return identifier;
-	}
+    /**
+     * create a new Muid type
+     * 
+     * @param identifier
+     *            identifier of the Muid type
+     */
+    private UidType(
+            final String identifier,
+            final short rawValue) {
+        this.identifier = identifier;
+        this.rawValue = rawValue;
+    }
 
-	/**
-	 * 
-	 * @param identifier
-	 *            identifier of the Muid type
-	 * @return Muid type having the identifier passed<br>
-	 *         <b>null</b> if no Muid type has such identifier
-	 */
-	public static UidType parseString(final String identifier) {
-		return (allTypesByString.get(identifier));
-	}
+    /**
+     * @return identifier of the Muid type
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
 
-	/**
-	 * Get a UidType by the type identifying number
-	 * 
-	 * @param identifier
-	 *            the number identifying the type.
-	 * @see MuidType.getMaximumAllowedType()
-	 * @return the enum identifying the Muid type corresponding to the given
-	 *         identifier
-	 * @throws UnknownMuidException
-	 *             thrown if the given identifier is invalid
-	 */
-	public static UidType parseShort(final short identifier)
-			throws UnknownMuidException {
-		if (identifier > UidType.values().length) {
-			throw new UnknownMuidException(identifier);
-		}
-		return UidType.values()[identifier];
-	}
+    /**
+     * @return short value identifying the Muid type
+     */
+    public short getRawIdentifier() {
+        return rawValue;
+    }
 
-	/**
-	 * Get a UidType by a Muid or UrlID
-	 * 
-	 * @param ID
-	 *            The UID (Muid or UrlID)
-	 * @return The enum identifying the Muid type corresponding to the given UID
-	 * @throws UnknownMuidException
-	 *             Thrown if the given ID is invalid
-	 */
-	public static UidType parseId(final long ID) throws UnknownMuidException {
-		short identifier = UidConverter.getType(ID);
-		if (identifier > UidType.values().length) {
-			throw new UnknownMuidException(identifier);
-		}
-		return UidType.values()[identifier];
-	}
+    @Override
+    public String toString() {
+        return identifier;
+    }
 
-	/**
-	 * 
-	 * @return the largest muid type value allowed
-	 */
-	public static short getLargestAllowedType() {
-		return (short) UidType.values().length;
-	}
+    /**
+     * 
+     * @param identifier
+     *            identifier of the Muid type
+     * @return Muid type having the identifier passed<br>
+     *         <b>null</b> if no Muid type has such identifier
+     */
+    public static UidType parseString(final String identifier) {
+        return (allTypesByString.get(identifier));
+    }
+
+    /**
+     * Get a UidType by the type identifying number
+     * 
+     * @param identifier
+     *            the number identifying the type.
+     * @see MuidType.getMaximumAllowedType()
+     * @return the enum identifying the Muid type corresponding to the given
+     *         identifier
+     * @throws UnknownMuidException
+     *             thrown if the given identifier is invalid
+     */
+    public static UidType parseShort(final short identifier)
+            throws UnknownMuidException {
+        if (identifier > UidType.values().length) {
+            throw new UnknownMuidException(identifier);
+        }
+        return UidType.values()[identifier];
+    }
+
+    /**
+     * Get a UidType by a Muid or UrlID
+     * 
+     * @param ID
+     *            The UID (Muid or UrlID)
+     * @return The enum identifying the Muid type corresponding to the given UID
+     * @throws UnknownMuidException
+     *             Thrown if the given ID is invalid
+     */
+    public static UidType parseId(final long ID) throws UnknownMuidException {
+        short identifier = UidConverter.getType(ID);
+        if (identifier > UidType.values().length) {
+            throw new UnknownMuidException(identifier);
+        }
+        return UidType.values()[identifier];
+    }
+
+    /**
+     * 
+     * @return the largest muid type value allowed
+     */
+    public static short getLargestAllowedType() {
+        return (short) UidType.values().length;
+    }
 }
