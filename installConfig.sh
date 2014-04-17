9b25bea4c1cdbd12459df7625d2fe40f6a2eabb5
Jonas Kunze <jonas@metalcon.de>
2014-04-17 21:07:30 +0200

    Added install scripts

---
+++ installConfig.sh
@@ -0,0 +1,7 @@
+# path to server config
+CONFIG_PATH="/usr/share/metalcon/uid/config.txt"
+# directory for server files
+SERVER_DIR="/usr/share/metalcon/uid/urlDB"
+
+# rights for server directory
+SERVER_DIR_RIGHTS="`whoami`:`whoami`"
